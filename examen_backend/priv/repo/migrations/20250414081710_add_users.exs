5948c78ad2fe2fb3b0f330b6124b11af749e3e2c
MatildaAK <matilda.kallstrom@hotmail.se>
2025-04-25 15:02:50 +0200

    User table

--- examen_backend/priv/repo/migrations/20250414081710_add_users.exs
+++ examen_backend/priv/repo/migrations/20250414081710_add_users.exs
@@ -2,13 +2,30 @@ defmodule ExamenBackend.Repo.Migrations.AddUsers do
   use Ecto.Migration
 
   def change do
-    create table(:users) do
-      add :name, :string
-      add :email, :string
+    execute "CREATE EXTENSION IF NOT EXISTS citext", ""
 
-      timestamps()
+    create table(:users, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :user_name, :string, null: false
+      add :name, :string, null: false
+      add :email, :citext, null: false
+      add :hashed_password, :string, null: false
+      timestamps(type: :utc_datetime_usec)
     end
 
     create unique_index(:users, [:email])
+    create unique_index(:users, [:name])
+
+    create table(:users_tokens, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :user_id, references(:users, type: :binary_id, on_delete: :delete_all), null: false
+      add :token, :binary, null: false
+      add :context, :string, null: false
+      add :sent_to, :string
+      timestamps(type: :utc_datetime_usec, updated_at: false)
+    end
+
+    create index(:users_tokens, [:user_id])
+    create unique_index(:users_tokens, [:context, :token])
   end
 end
