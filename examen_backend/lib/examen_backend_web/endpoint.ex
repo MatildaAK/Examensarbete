2e1012a48baed18c9a8f5dcf4358574ccc8bfb4d
MatildaAK <matilda.kallstrom@hotmail.se>
2025-04-29 10:31:55 +0200

    commented out CheckRepoStatus

--- examen_backend/lib/examen_backend_web/endpoint.ex
+++ examen_backend/lib/examen_backend_web/endpoint.ex
@@ -29,7 +29,7 @@ defmodule ExamenBackendWeb.Endpoint do
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
     plug Phoenix.CodeReloader
-    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :examen_backend
+    # plug Phoenix.Ecto.CheckRepoStatus, otp_app: :examen_backend
   end
 
   plug Phoenix.LiveDashboard.RequestLogger,
