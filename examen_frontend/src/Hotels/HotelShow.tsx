7f28d1a4c688462f7e629125b83c3ca32b0e7c84
MatildaAK <matilda.kallstrom@hotmail.se>
2025-04-30 15:10:05 +0200

    create and get hotelroom in hotel

--- examen_frontend/src/Hotels/HotelShow.tsx
+++ examen_frontend/src/Hotels/HotelShow.tsx
@@ -1,22 +1,27 @@
 import { useNavigate, useParams } from "react-router-dom";
-import { Hotel } from "../Interfaces/Hotel";
+import { Hotel, HotelRoom, NewHotelRoom } from "../Interfaces/HotelTypes";
 import { useEffect, useState } from "react";
 import BASE_URL from "../config";
 import Container from "../components/core/Container";
 import Back from "../components/core/Back";
-import List from "../components/core/List";
 import Button from "../components/core/Button";
 import Modal from "../components/core/Modal";
 import HotelForm from "./HotelFormComponent";
 import { updateHotel } from "./Hotel";
+import Table from "../components/core/Table";
+import HotelRoomForm from "../Hotel_Rooms/HotelRoomFormComponent";
+import { createHotelRoom } from "../Hotel_Rooms/HotelRooms";
 
 const HotelShow = () => {
   const { id } = useParams<{ id: string }>();
-  const [hotel, setHotel] = useState<Hotel | null>(null);
+  const [hotels, setHotels] = useState<Hotel | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const navigate = useNavigate();
   const [modalOpen, setModalOpen] = useState(false);
+  const [roomModalOpen, setRoomModalOpen] = useState(false);
+  const [_selectedHotel, setSelectedHotel] = useState<Hotel | undefined>(undefined);
+  const [hotelRooms, setHotelRooms] = useState<HotelRoom[]>([]);
 
   useEffect(() => {
     if (!id) return;
@@ -36,11 +41,24 @@ const HotelShow = () => {
         return res.json();
       })
       .then((data) => {
-        setHotel(data.data);
+        setHotels(data.data);
         setLoading(false);
       })
       .catch((err) => {
         setError(err.message);
+        setLoading(false);
+      });
+
+      fetch(`${BASE_URL}/hotel_rooms`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "X-Tenant": `${tenant}`,
+        },
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        setHotelRooms(data.data);
         setLoading(false);
       });
   }, [id]);
@@ -52,15 +70,21 @@ const HotelShow = () => {
     }
 
     const updated = await updateHotel(hotelToSave.id, hotelToSave.name);
-    setHotel(updated);
+    setHotels(updated);
     setModalOpen(false);
   };
 
+    const handleCreateHotelRoom = async (newHotelRoom: NewHotelRoom) => {
+      const created = await createHotelRoom(newHotelRoom.name, newHotelRoom.size, newHotelRoom.hotel_id);
+      setHotelRooms((prev) => [...prev, created]);
+      setRoomModalOpen(false);
+    };
+
   if (loading) return <p>Laddar hotell...</p>;
   if (error) return <p>Något gick fel: {error}</p>;
-  if (!hotel) return <p>Ingen hotell hittades.</p>;
+  if (!hotels) return <p>Ingen hotell hittades.</p>;
 
-  const deleteHotel = async (id: number) => {
+  const deleteHotel = async (id: string) => {
     const confirmDelete = window.confirm(
       "Är du säker på att du vill ta bort detta hotell?"
     );
@@ -86,19 +110,51 @@ const HotelShow = () => {
         <Back>Tillbaka till Lista</Back>
       </div>
       <h1 className="text-2xl font-bold mb-4 text-start">
-        Hotell: {hotel.name}
+        Hotell: {hotels.name}
       </h1>
-      <List
-        items={[
-            { title: "Namn", value: hotel.name },
-            { title: "Hotell rum",  value: hotel.hotel_rooms?.map((room) => room.name).join(", ") || "Inga rum",}
+      <div className="p-6 flex justify-end">
+        <Button
+          onClick={() => setRoomModalOpen(true)}
+          type="button"
+          variant="primary"
+          size="small"
+        >
+          Nytt hotellrum
+        </Button>
+
+        <Modal
+          isOpen={roomModalOpen}
+          onClose={() => setRoomModalOpen(false)}
+          title={"Skapa Hotellrum"}
+        >
+          <HotelRoomForm<NewHotelRoom>
+            onSubmit={handleCreateHotelRoom}
+            submitText="Skapa hotelrum" 
+            initialHotelId={hotels.id.toString()}         
+          />
+        </Modal>
+      </div>
+      <Table
+        items={hotelRooms.filter((hotel_room) => hotel_room.hotel_id === id)}
+        columns={[
+          {
+            label: "Namn",
+            render: (hotel_room) => hotel_room.name,
+          },
+          {
+            label: "Storlek",
+            render: (hotel_room) => hotel_room.size,
+          },
         ]}
-        renderSlot={(item) => item.value}
+        onRowClick={(hotel_room) => {
+          setSelectedHotel(hotel_room);
+          navigate(`/hotel_rooms/${hotel_room.id}`, { state: { hotel_room } });
+        }}
       />
 
       <div className="flex justify-end mt-8 gap-6">
         <Button
-          onClick={() => deleteHotel(hotel.id)}
+          onClick={() => deleteHotel(hotels.id)}
           type="button"
           size="small"
           variant="danger"
@@ -122,7 +178,7 @@ const HotelShow = () => {
           title={"Redigera hotell"}
         >
           <HotelForm<Hotel>
-            initialHotel={hotel}
+            initialHotel={hotels}
             onSubmit={handleSaveHotel}
             submitText="Uppdatera"
           />
