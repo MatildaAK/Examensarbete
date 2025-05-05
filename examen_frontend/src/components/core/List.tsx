172f1ad227427b1a2dc019ce1b7ca9ce188f9e54
MatildaAK <matilda.kallstrom@hotmail.se>
2025-05-05 11:13:54 +0200

    removed unused colors

--- examen_frontend/src/components/core/List.tsx
+++ examen_frontend/src/components/core/List.tsx
@@ -6,11 +6,11 @@ interface ListProps {
 const List: React.FC<ListProps> = ({ items, renderSlot }) => {
   return (
     <div className="mt-14">
-      <dl className="divide-border -my-4 divide-y">
+      <dl className="-my-4 divide-y">
         {items.map((item, i) => (
-          <div key={i} className="flex gap-4 py-4 text-sm leading-6 border sm:gap-8">
-            <dt className="w-1/4 flex-none text-accent0">{item.title}</dt>
-            <dd className="text-accent-foreground">{renderSlot(item)}</dd>
+          <div key={i} className="flex gap-4 py-4 text-sm leading-6 sm:gap-8">
+            <dt className="w-1/4 flex-none">{item.title}</dt>
+            <dd>{renderSlot(item)}</dd>
           </div>
         ))}
       </dl>
